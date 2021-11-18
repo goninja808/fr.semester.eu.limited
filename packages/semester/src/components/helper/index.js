05b19df05405f86975eecc7f30f4895c7fe29087
goninja808 <goninja808@gmail.com>
2021-11-18 11:09:48 +0100

    env var for region

--- packages/semester/src/components/helper/index.js
+++ packages/semester/src/components/helper/index.js
@@ -1,8 +1,6 @@
 import {categoriesWidgetsHome} from '../config'
-require('dotenv').config();
 const MAXIMUM_POSTS = 5  
-const NODE_TAGID = process.env.NODE_TAGID;
-console.log(`Your port is ${port}`);
+const NODE_TAGID =  (!!(process.env.NODE) ? process.env.NODE : "47");
 const getPostsFromCategory = ({ post }, categoryId, _tagId) =>
   Object.keys(post)
     .map(postID => post[postID])
@@ -11,7 +9,7 @@ const getPostsFromCategory = ({ post }, categoryId, _tagId) =>
     export const getPostsGroupedByCategory = source =>  {
       return Object.values(categoriesWidgetsHome)
         .reduce((acc, categoryId) => {
-          const posts = getPostsFromCategory(source, categoryId,TAG_ID).slice(0,MAXIMUM_POSTS)
+          const posts = getPostsFromCategory(source, categoryId,NODE_TAGID).slice(0,MAXIMUM_POSTS)
           const category = source.category[categoryId]
           return [...acc, {posts, category}]
         }, [])
