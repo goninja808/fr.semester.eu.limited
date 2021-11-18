34abd4a77c78c3e7325c098ff9b409f5d99f9d62
goninja808 <goninja808@gmail.com>
2021-11-18 17:41:14 +0100

    obtain env variable

--- packages/semester/src/components/helper/index.js
+++ packages/semester/src/components/helper/index.js
@@ -1,6 +1,6 @@
 import {categoriesWidgetsHome} from '../config'
 const MAXIMUM_POSTS = 5  
-const NODE_TAGID =  (!!(process.env.NODE) ? process.env.NODE : "47");
+const TAGID =  (!!(process.env.TAGID) ? process.env.TAGID : "47");
 const getPostsFromCategory = ({ post }, categoryId, _tagId) =>
   Object.keys(post)
     .map(postID => post[postID])
@@ -9,7 +9,7 @@ const getPostsFromCategory = ({ post }, categoryId, _tagId) =>
     export const getPostsGroupedByCategory = source =>  {
       return Object.values(categoriesWidgetsHome)
         .reduce((acc, categoryId) => {
-          const posts = getPostsFromCategory(source, categoryId,NODE_TAGID).slice(0,MAXIMUM_POSTS)
+          const posts = getPostsFromCategory(source, categoryId,TAGID).slice(0,MAXIMUM_POSTS)
           const category = source.category[categoryId]
           return [...acc, {posts, category}]
         }, [])
