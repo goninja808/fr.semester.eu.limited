db8d9b531c07b0f0f405d710a44291e6b1d032ed
goninja808 <goninja808@gmail.com>
2021-12-14 07:07:12 +0100

    copied vers on 2021-12-14 07:07:12

---
+++ packages/semester/src/components/config_tag.js
@@ -0,0 +1,23 @@
+const idR202201 = 46;
+const idR202202 = 47;
+const idR202203 = 52;
+const idR202204 = 49;
+const idR202205 = 50;
+const idR202206 = 51; 
+
+
+export const idEvents = 70;
+const idFrance = 92; 
+
+
+export const MonthRegionTags = [idR202201,idR202202,idR202203,idR202204,idR202205,idR202205]
+ 
+
+// {
+//     "Centre Val de Loire, Bourgogne-Franche-Comté": 46,
+//     "Auvergne-Rhône-Alpes, Provence-Alpes-Côte d’Azur, Corse": 47,
+//     "Hauts de France, Ile de de France, Grand Est": 88,
+//     "DROM, Départements et Régions d’Outre-Mer": 89,
+//     "Normandie Bretagne PaysdeLoire":90,
+//     "Nouvelle Aquitaine, Occitanie":91
+//   }
---
+++ packages/semester/src/components/stat-post.js
@@ -0,0 +1,259 @@
+import {useEffect, useRef } from "react";
+import { connect, styled } from "frontity";
+import Link from "./link"; 
+import HeaderMedia from "./header-media";
+import { getFactsForRegion, getEventsForRegion } from "./helper";    
+import {eventCategory} from "./config"
+import Switch from "@frontity/components/switch"; 
+import React,{useState} from "react"
+
+/**
+ * The Post component that Mars uses to render any kind of "post type", like
+ * posts, pages, attachments, etc.
+ *
+ * It doesn't receive any prop but the Frontity store, which it receives from
+ * {@link connect}. The current Frontity state is used to know which post type
+ * should be rendered.
+ *
+ * @param props - The Frontity store (state, actions, and libraries).
+ *
+ * @example
+ * ```js
+ * <Switch>
+ *   <Post when={data.isPostType} />
+ * </Switch>
+ * ```
+ *
+ * @returns The {@link Post} element rendered.
+ */
+const PerSemiStaticPost = ({ state, actions, libraries ,tagId}) => {
+  // Get information about the current URL.
+  const data = state.source.get(state.router.link);
+  const resultEvent = getEventsForRegion(state.source,tagId);
+  const resultFact = getFactsForRegion (state.source,tagId) ;
+  // Get the html2react component.
+  const Html2React = libraries.html2react.Component;
+ 
+  /**
+   * Once the post has loaded in the DOM, prefetch both the
+   * home posts and the list component so if the user visits
+   * the home page, everything is ready and it loads instantly.
+   */
+  // Load the post, but only if the data is ready.
+  
+  return data.isReady ? (
+    <FlexContainer>
+         <Switch>
+         <Container when={state.router.link=='/regionofthemonth/'}>
+         { 
+         resultEvent.map(({ posts, category, isNotHeader }, index) => (
+            <CategoryGP key={index} className={`GroupCategory col-12 align-self-strech  count${posts.length}`} >
+              <HeadingGroupCategory  className={`${category.slug} `}>  <Illust src={`/static/images/${category.slug}_picto.png`} title={category.link}/> {category.name}</HeadingGroupCategory>
+                <div className="GroupCategory-box col-md-12">
+                 {posts.map((post, index) => (
+                  <article key={index}>
+                    <div>
+                        <div px={2}>
+                         {  <Link link={post.link}>
+                            <h2>
+                           <Html2React html={post.title.rendered} /> 
+                            </h2>
+                          </Link> }
+                          { !(isNotHeader) ? <HeaderMedia id={post.featured_media} /> : null}
+                          <Html2React html={post.excerpt.rendered} />
+                        </div>                  
+                    </div>
+                  </article>
+                  ))}
+                  </div>
+                  {isNotHeader?<Link link={category.link}>
+                  <p>&gt;&gt; See more <strong>{category.name}</strong> related posts </p>
+                </Link>:null}
+            </CategoryGP>
+          ))}
+          <span/>
+          {resultFact.map(({ posts, category, isNotHeader }, index) => (
+            <CategoryGP key={index} className={`GroupCategory col-12 align-self-strech  count${posts.length}`} >
+              <HeadingGroupCategory  className={`${category.slug} `}>  <Illust src={`/static/images/${category.slug}_picto.png`} title={category.link}/> {category.name}</HeadingGroupCategory>
+                <div className="GroupCategory-box col-md-12">
+                 {posts.map((post, index) => (
+                  <article key={index}>
+                    <div>
+                        <div px={2}>
+                         {  <Link link={post.link}>
+                            <h2>
+                           <Html2React html={post.title.rendered} /> 
+                            </h2>
+                          </Link> }
+                          { !(isNotHeader) ? <HeaderMedia id={post.featured_media} /> : null}
+                          <Html2React html={post.excerpt.rendered} />
+                        </div>                  
+                    </div>
+                  </article>
+                  ))}
+                  </div>
+                  {isNotHeader?<Link link={category.link}>
+                  <p>&gt;&gt; See more <strong>{category.name}</strong> related posts </p>
+                </Link>:null}
+            </CategoryGP>
+          ))
+         }
+           </Container>
+           
+           <Container when={state.router.link=='/'}>
+            { resultFact.map(({ posts, category, isNotHeader }, index) => (
+            <CategoryGP key={index} className={`GroupCategory col-12 align-self-strech  count${posts.length}`} >
+              <HeadingGroupCategory  className={`${category.slug} `}>  <Illust src={`/static/images/${category.slug}_picto.png`} title={category.link}/> {category.name}</HeadingGroupCategory>
+                <div className="GroupCategory-box col-md-12">
+                 {posts.map((post, index) => (
+                  <article key={index}>
+                    <div>
+                        <div px={2}>
+                         {  <Link link={post.link}>
+                            <h2>
+                           <Html2React html={post.title.rendered} /> 
+                            </h2>
+                          </Link> }
+                          { !(isNotHeader) ? <HeaderMedia id={post.featured_media} /> : null}
+                          <Html2React html={post.excerpt.rendered} />
+                        </div>
+                      
+                    </div>
+                  </article>
+                  ))}
+                  </div>
+                  {isNotHeader?<Link link={category.link}>
+                  <p>&gt;&gt; See more <strong>{category.name}</strong> related posts </p>
+                </Link>:null}
+            </CategoryGP>
+          ))
+         }
+           </Container>
+         </Switch>
+    </FlexContainer>
+  ) : null;
+};
+
+export default connect(PerSemiStaticPost);
+const FlexContainer = styled.div`
+  display: flex;
+`
+const Illust = styled.img`
+  max-width: 50px;
+  border-radius: 25px;
+  margin-right: 25px;
+`;
+
+const BigImage = styled.img`
+  max-width: 320px;
+  border-radius: 25px;
+  margin-right: 25px;
+`;
+
+
+const Container = styled.section`
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  grid-gap: 8px;
+  background-color: #fff;
+  color: #444;  
+  min-width: 400px;
+  margin: 0 auto;
+  padding-right: 8px;
+  padding-left: 8px;
+  list-style: none;
+  @media (max-width: 800px) {
+    display: grid;
+  grid-template-columns: repeat(1, 1fr);
+  grid-gap: 10px;
+  background-color: #fff;
+  color: #444;  
+  min-width: 400px;
+  margin: 0 auto;
+  padding-right: 10px;
+  padding-left: 10px;
+  list-style: none;
+}
+`;
+
+const HeadGroupCategory = styled.article`
+  max-width:771px;
+  margin:0 auto;
+  position: relative;
+  margin-bottom:3.5rem;
+  .article-title {    
+    &:hover {
+      h1 {
+        color:var(--brand);
+      }
+    }
+  }
+`;
+ 
+
+const CategoryGP = styled.article`
+max-width:771px;
+margin:0 auto;
+position: relative;
+/**Job articles**/
+&.count0{
+  visibility: hidden;
+  display: none;
+}
+&.newscategory {
+  max-width: 100%;
+  margin: 0;
+  margin-bottom: 1 rem;
+  display: flex;
+  flex-direction: column;
+  .categorybox {
+    padding: 2rem;
+    background: var(--grey);
+    box-shadow: 0px 2px 16px -9px rgba(0,0,0,0.5);
+    border: 1px solid #ececec;
+    border-radius:5px;
+    transition: all .4s ease;
+    display: flex;
+    flex-grow: 1;      
+    flex-direction: column;
+    .articletitle {
+      text-decoration:none;
+      h4 {
+        transition: all .3s ease;
+      }        
+      &:hover {
+        h4 {
+          color:var(--brand);
+        }          
+      }
+    }
+  }
+}
+`;
+
+const Header = styled.h3`
+  text-align:left;
+  margin-bottom:1rem;
+  margin-left:1rem;
+`;
+
+const HeadingGroupCategory = styled.h2`
+  font-size: 60px;
+  padding: 5px;
+  &.header{
+    background-color: white;
+    display: none;
+  }  
+  &.culture{
+    background-color: #fff2cc;
+  }
+  &.initiative{
+    background-color: #f4cccc;
+  }
+  &.lifestyle{
+    background-color: #cfe2f3;
+  }
+  &.science{
+    background-color: #d9ead3;
+  }
+`
