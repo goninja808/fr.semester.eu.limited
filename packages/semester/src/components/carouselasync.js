dc7483c9cb79fa3eaf8c55e9b862361809c319ad
goninja808 <goninja808@gmail.com>
2022-01-29 14:07:31 +0100

    copied vers on 2022-01-29 14:07:31

--- packages/semester/src/components/carouselasync.js
+++ packages/semester/src/components/carouselasync.js
@@ -3,47 +3,47 @@ import { connect, styled } from "frontity";
 import ReactPlayer from "react-player/lazy";
 import Carousel from 'react-bootstrap/Carousel'
  
-const ComponentWithDimensions = props => {
-    const targetRef = useRef();
-    const [dimensions, setDimensions] = useState({ width: 0, heigth: 0 });
+// const ComponentWithDimensions = props => {
+//     const targetRef = useRef();
+//     const [dimensions, setDimensions] = useState({ width: 0, heigth: 0 });
   
-    // holds the timer for setTimeout and clearInterval
-    let movement_timer = null;
+//     // holds the timer for setTimeout and clearInterval
+//     let movement_timer = null;
   
-    // the number of ms the window size must stay the same size before the
-    // dimension state variable is reset
-    const RESET_TIMEOUT = 100;
+//     // the number of ms the window size must stay the same size before the
+//     // dimension state variable is reset
+//     const RESET_TIMEOUT = 100;
   
-    const test_dimensions = () => {
-      // For some reason targetRef.current.getBoundingClientRect was not available
-      // I found this worked for me, but unfortunately I can't find the
-      // documentation to explain this experience
-      if (targetRef.current) {
-        setDimensions({
-          width: targetRef.current.offsetWidth,
-          height: targetRef.current.offsetHeight
-        });
-      }
-    };
+//     const test_dimensions = () => {
+//       // For some reason targetRef.current.getBoundingClientRect was not available
+//       // I found this worked for me, but unfortunately I can't find the
+//       // documentation to explain this experience
+//       if (targetRef.current) {
+//         setDimensions({
+//           width: targetRef.current.offsetWidth,
+//           height: targetRef.current.offsetHeight
+//         });
+//       }
+//     };
   
-    // This sets the dimensions on the first render
-    useLayoutEffect(() => {
-      test_dimensions();
-    }, []);
+//     // This sets the dimensions on the first render
+//     useLayoutEffect(() => {
+//       test_dimensions();
+//     }, []);
   
-    // every time the window is resized, the timer is cleared and set again
-    // the net effect is the component will only reset after the window size
-    // is at rest for the duration set in RESET_TIMEOUT.  This prevents rapid
-    // redrawing of the component for more complex components such as charts
-    window.addEventListener("resize", () => {
-      clearInterval(movement_timer);
-      movement_timer = setTimeout(test_dimensions, RESET_TIMEOUT);
-    });
+//     // every time the window is resized, the timer is cleared and set again
+//     // the net effect is the component will only reset after the window size
+//     // is at rest for the duration set in RESET_TIMEOUT.  This prevents rapid
+//     // redrawing of the component for more complex components such as charts
+//     window.addEventListener("resize", () => {
+//       clearInterval(movement_timer);
+//       movement_timer = setTimeout(test_dimensions, RESET_TIMEOUT);
+//     });
   
-    return (
-      <div ref={targetRef} maxHeight= {dimensions.width} maxHeight= {dimensions.height} > </div>
-    );
-  };
+//     return (
+//       <div ref={targetRef} maxHeight= {dimensions.width} maxHeight= {dimensions.height} > </div>
+//     );
+//   };
   
   
  
@@ -65,7 +65,7 @@ const CarouselAsynch = ({state, initMedia, preMedia, postMedia }) => {
     
     return (
         <>
-        <ComponentWithDimensions />
+        {/* <ComponentWithDimensions /> */}
             <SliderWrapper className="flex-container">
                 <div classe='wrappedContent' style={{ maxHeight: maxheight + 'px' }}>
                 {( preMedia || postMedia || (initMedia && initMedia !=0)) ? 
